18660716e3db94c526e206efa2eae2b506889ed9
semengHan <semeng_han@163.com>
2023-12-26 14:12:41 +0800

    fixed: 添加基础 layout

--- src/layout/BasicLayout.tsx
+++ src/layout/BasicLayout.tsx
@@ -1,20 +1,77 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { Space } from "antd";
+import { Layout, Space, Menu, Button } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+const { Header, Sider, Content } = Layout;
 import styles from "./index.module.less";
 
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: "hidden",
+};
+
 const BasicLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleSelect = () => {};
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <span className={styles.title}>React Map Gl</span>
-        <Space>
-          <Link to="/">Home</Link>
-          <Link to="articleList">ArticleList</Link>
-          <Link to="zustand">Zustand</Link>
-        </Space>
-      </div>
-      {/* Outlet相当于是子路由的占位符 */}
-      <Outlet />
+      <Layout style={layoutStyle}>
+        <Header className={styles.header}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+            }}
+          />
+          <span className={styles.title}>React Map Gl</span>
+        </Header>
+        <Layout>
+          <Sider
+            collapsed={collapsed}
+            collapsible
+            collapsedWidth={50}
+            width={200}
+            trigger={null}
+            className={styles.menu}
+          >
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              onSelect={handleSelect}
+              items={[
+                {
+                  key: "1",
+                  icon: <UserOutlined />,
+                  label: <Link to="/">Home</Link>,
+                },
+                {
+                  key: "2",
+                  icon: <VideoCameraOutlined />,
+                  label: <Link to="articleList">ArticleList</Link>,
+                },
+                {
+                  key: "3",
+                  icon: <UploadOutlined />,
+                  label: <Link to="zustand">Zustand</Link>,
+                },
+              ]}
+            />
+          </Sider>
+          <Content className={styles.container}>
+            {/* Outlet相当于是子路由的占位符 */}
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 };
